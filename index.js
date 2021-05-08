a42f9fa1216688ef4da58f8b3a3d2ef3a47ca5de
Ignas <IgnasXP@gmail.com>
2021-05-08 19:22:05 +0100

    changed sql

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 const express = require('express');
+const { createPool } = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-    console.log("aaa");
-   
-    var sql = require("mssql");
-
-    // config for your database
-    var config = {
-        user: 'sa',
-        password: 'mypassword',
-        server: 'localhost', 
-        database: 'SchoolDB' 
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from Student', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
+const pool = createPool({
+    host:'eu-cdbr-west-01.cleardb.com',
+    user:'b06e98fcde28f0',
+    password:'cd51e4b0',
+    database:'<dbname>',
+    connectionLimit:10
 });
+
+pool.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
 
 var server = app.listen(5000, function () {
     console.log(`Server running on port ${port}, http://localhost:${port}`);
